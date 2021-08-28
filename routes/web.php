570f7f6fad6b64f57a1c0846615b0c738aaa912f
jun.otsuka <badmintonjun0528@icloud.com>
2021-08-28 11:31:39 +0000

    commit2

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-// CRUD
-// メッセージの個別詳細ページ表示
-Route::get('tasks/{id}', 'TasksController@show');
-// メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
-Route::post('tasks', 'TasksController@store');
-// メッセージの更新処理（編集画面を表示するためのものではありません）
-Route::put('tasks/{id}', 'TasksController@update');
-// メッセージを削除
-Route::delete('tasks/{id}', 'TasksController@destroy');
 Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
-Route::get('tasks/{id}/copy', 'TasksController@copy');
-// index: showの補助ページ
-Route::get('tasks', 'TasksController@index')->name('tasks.index');
-Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
-Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
 
