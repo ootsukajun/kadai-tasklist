a05a679ed98c916770e3497f279978fd79ef09e1
jun.otsuka <badmintonjun0528@icloud.com>
2021-09-09 09:59:13 +0000

    top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
         @include('commons.navbar')
 
         <div class="container">
+            {{-- エラーメッセージ --}}
              @include('commons.error_tasks')
             @yield('content')
         </div>
